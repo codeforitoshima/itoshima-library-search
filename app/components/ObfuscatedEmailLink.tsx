14f7d9fa6c17223825a6d230f7b36bd7371bf45d
Alejandro Cremades <ale@alecrem.com>
2026-04-04 14:28:48 +0900

    Usar button en vez de anchor en ObfuscatedEmailLink

--- app/components/ObfuscatedEmailLink.tsx
+++ app/components/ObfuscatedEmailLink.tsx
@@ -14,11 +14,10 @@ export function ObfuscatedEmailLink({ user, domain, className }: ObfuscatedEmail
   }, [user, domain]);
 
   return (
-    <a
-      href="#"
+    <button
+      type="button"
       className={className}
-      onClick={(e) => {
-        e.preventDefault();
+      onClick={() => {
         window.location.href = `mailto:${user}@${domain}`;
       }}
     >
@@ -26,6 +25,6 @@ export function ObfuscatedEmailLink({ user, domain, className }: ObfuscatedEmail
         <path d="M1.5 2h13A1.5 1.5 0 0 1 16 3.5v9A1.5 1.5 0 0 1 14.5 14h-13A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2zm0 1a.5.5 0 0 0-.5.5v.77l7 4.2 7-4.2V3.5a.5.5 0 0 0-.5-.5h-13zM15 5.64l-6.54 3.93a.75.75 0 0 1-.92 0L1 5.64V12.5a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5V5.64z" />
       </svg>
       <span ref={labelRef} />
-    </a>
+    </button>
   );
 }
